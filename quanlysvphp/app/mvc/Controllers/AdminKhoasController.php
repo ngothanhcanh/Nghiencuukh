49aef09cb1f02bdc070f6e582b4855d7dacbddaf
hoanhtu02 <109658763+hoanhtu02@users.noreply.github.com>
2023-06-07 21:42:32 +0700

    phu huynh, khoas

---
+++ quanlysvphp/app/mvc/Controllers/AdminKhoasController.php
@@ -0,0 +1,41 @@
+<?php 
+class AdminKhoasController extends Controller
+{   private $khoasModel;
+    public function __construct()
+    {
+        $this->khoasModel=$this->model('KhoasModel');
+    }
+    public function index() 
+    {  
+        if(isset($_GET['delete']))
+        {    $id=$_GET['delete'];
+            
+            if($this->khoasModel->delete($id))
+            {
+            header('location:'.URL.'/AdminKhoaHocController/index');
+            }
+        }
+        $result=$this->khoasModel->show();
+        $this->view('admin/khoahoc',
+    [
+        'result'=>$result,
+    ]);
+    }
+    public function save()
+    {    
+       
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+       
+    // Lấy dữ liệu từ yêu cầu AJAX
+    $id = $_POST["id"];
+    $this->khoasModel->add($id);
+    // TODO: Thực hiện lưu dữ liệu vào cơ sở dữ liệu
+    // Gọi phương thức lưu dữ liệu trong khoasModel
+    // Trả về kết quả (ví dụ: thành công hoặc thất bại)
+    $response = array(
+        'id'=>$id,
+    );
+    echo json_encode($response);
+      }
+    }
+}
---
+++ quanlysvphp/app/mvc/models/KhoasModel.php
@@ -0,0 +1,44 @@
+<?php 
+class KhoasModel
+{  private $db;
+    public function __construct()
+    {
+        $this->db=new Database();
+    }
+    public function showid()
+    {
+        $sql = "SELECT * FROM khoas ";
+        $resuil=$this->db->execute($sql);
+        if(mysqli_num_rows($resuil)>0)
+        {
+            $data= $resuil->fetch_all(MYSQLI_ASSOC);
+            return $data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public function show()
+    {
+        $sql="SELECT * FROM khoas ORDER BY ID ASC";
+        $resuil= $this->db->execute($sql);
+        if(mysqli_num_rows($resuil)>0)
+        {
+            $data=$resuil->fetch_all(MYSQLI_ASSOC);    
+            return $data;
+        }
+    }
+    public function add($id)
+    {   
+          $sql="INSERT INTO `khoas`(`ID`)
+            VALUES ('$id') ";
+        return $this->db->execute($sql); 
+    }
+    public function delete($id)
+    {
+       $sql= "DELETE FROM `khoas` WHERE id='$id'";
+       $resuil= $this->db->execute($sql);
+       return $resuil;
+    }
+}
